2fff58b13ed79abbcdfa5d78cb577e329b0da90a
xxeniaev <xenia-ev2502@yandex.ru>
2021-04-24 20:47:54 +0500

    upd: multiprocessing

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
 import sys
 import socket
+import time
 from datetime import datetime
+import multiprocessing.dummy as mp
 
 
-# Defining a target
-# translate hostname to IPv4
-target = socket.gethostbyname(sys.argv[1])
-
-# Add Banner 
-print("Scanning Target: " + target)
-print("Scanning started at:" + str(datetime.now()))
-print("-" * 50)
-
-
-try:
-
+def scan(port):
     # will scan ports between 1 to 65 535
-    for port in range(1, 5535):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
 
-        # returns an error indicator
-        result = s.connect_ex((target, port))
-        if result == 0:
-            print("Port {} is open".format(port))
-        s.close()
+    # returns an error indicator
+    result = s.connect_ex((target, port))
+    if result == 0:
+        print("Port {} is open".format(port))
 
-except KeyboardInterrupt:
-    print("\n Exitting Program !!!!")
-    sys.exit()
-except socket.gaierror:
-    print("\n Hostname Could Not Be Resolved !!!!")
-    sys.exit()
-except socket.error:
-    print("\ Server not responding !!!!")
-    sys.exit()
+    if port % 10 == 0:
+        print(port)
 
-print("-" * 50)
-print("Done")
+    s.close()
+
+
+if __name__ == '__main__':
+    # Defining a target
+    # translate hostname to IPv4
+    target = socket.gethostbyname(sys.argv[1])
+
+    # Add Banner
+    print("Scanning Target: " + target)
+    print("Scanning started at:" + str(datetime.now()))
+    print("-" * 50)
+
+    seconds = int(round(time.time()))
+
+    try:
+        p = mp.Pool(4)
+        p.map(scan, range(1, 50))  # range(0,1000) if you want to replicate your example
+        p.close()
+        p.join()
+
+    except KeyboardInterrupt:
+        print("\n Exitting Program !!!!")
+        sys.exit()
+    except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+    except socket.error:
+        print("\n Server not responding !!!!")
+        sys.exit()
+
+    print("-" * 50)
+    print('All work completed', round(time.time()) - seconds, "sec")
